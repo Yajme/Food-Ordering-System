c738479640854ac8821ce274ea586ac8f019270a
Yajme <spartan.emjayrongavilla@gmail.com>
2023-12-05 17:43:06 +0800

    added product models

--- model/productmodel.php
+++ model/productmodel.php
@@ -1,12 +1,54 @@
 <?php
-include_once('../db/connection.php');
+include_once('../../db/connection.php');
 //SELECT Category, COUNT(Category) as Products FROM `view_availableproducts` GROUP BY Category
 class ProductModel extends Database {
     public function __construct(){
         parent::__construct();
     }
 
+    public function selectAllProducts(){
+        $query = "SELECT * FROM view_availableproducts";
+        $rows = $this->read($query);
+        if(!$rows) throw new Exception("No products found");
+        return $rows;
+    }
 
+    public function selectProductByCategory($category){
+        $category = $this->escape_string($category);
+        $query = "SELECT * FROM view_availableproducts WHERE category = '$category'";
+        $rows = $this->read($query);
+        if(!$rows) throw new Exception("No products found");
+        return $rows;
+    }
+
+    public function selectProductBySearch($search){
+        $search = $this->escape_string($search);
+        $query = "SELECT * FROM view_availableproducts WHERE productname LIKE '%$search%'";
+        $rows = $this->read($query);
+        if(!$rows) throw new Exception("No products found");
+        return $rows;
+    }
+
+    public function loadCategories(){
+        $query = "SELECT Category, COUNT(Category) as Products, image_path FROM `view_availableproducts` GROUP BY Category";
+        $rows = $this->read($query);
+        if(!$rows) throw new Exception("No products found");
+        return $rows;
+    }
+
+    public function loadFeaturedProducts(){
+        $query = "SELECT * FROM view_featured_products ORDER BY RAND()";
+        $rows = $this->read($query);
+        if(!$rows) throw new Exception("No products found");
+        return $rows;
+    }
+
+    public function recentProducts(){
+        $query = "SELECT * FROM view_availableproducts ORDER BY created_at DESC LIMIT 5";
+        $rows = $this->read($query);
+        if(!$rows) throw new Exception("No products found");
+        return $rows;
+    }
 }
 
 ?>
