64c0cb9f36f22ad549aad68db3d342c8ece51fe7
Yajme <spartan.emjayrongavilla@gmail.com>
2023-12-05 17:42:33 +0800

    Fixed logout clear cookies

--- views/user/logout.php
+++ views/user/logout.php
@@ -2,13 +2,16 @@
 /**
  * Script only for unsetting Session;
  */
+ob_start(); // Turn on output buffering
 session_start();
 
 if (isset($_POST['logout'])) {
     unset($_SESSION['user_name']);
+    setcookie('userid', '', time() - 3600, "/"); // set the cookie value to empty string
+    setcookie('customerid', '', time() - 3600, "/");
     // Redirect to the desired page after logout
     header('Location: index.php');
     exit();
 }
-
+ob_end_flush(); // Send the output buffer and turn off output buffering
 ?>
