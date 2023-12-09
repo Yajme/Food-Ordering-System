f627e123fcaee36e3b1a61163800fdd7360909ef
DzeyEm <jmlaylo7@gmail.com>
2023-12-10 03:01:27 +0800

    Order page done CRUD and Bind statement working

--- db/connection.php
+++ db/connection.php
@@ -4,7 +4,6 @@ interface IFunctions{
     public function execute($query);
     public function read($query);
     public function escape_string($value);
-    
 }
 abstract class Database implements IFunctions
 {
@@ -73,5 +72,36 @@ abstract class Database implements IFunctions
         return $escapedValue;
     }
     
+    public function statementBind($sql, $params) {
+        // Prepare the query
+        $sqlstmt = $sql;
+        $stmt = mysqli_prepare($this->connection, $sqlstmt);
+        
+        $parameters = array();
+        $types = ''; // Variable to store parameter types
+    
+        foreach ($params as $key => $value) {
+            // Build the types string based on the type of each parameter
+            if (is_int($value)) {
+                $types .= 'i'; // 'i' represents integer
+            } elseif (is_double($value)) {
+                $types .= 'd'; // 'd' represents double
+            } elseif (is_string($value)) {
+                $types .= 's'; // 's' represents string
+            } else {
+                $types .= 'b'; // 'b' represents BLOB
+            }
+    
+            $parameters[] = &$params[$key]; // Pass parameters by reference
+        }
+    
+        // Bind parameters
+        mysqli_stmt_bind_param($stmt, $types, ...$params);
+    
+        // Execute the query
+        return mysqli_stmt_execute($stmt);
+    }
+    
 }
+
 ?>
--- views/admin/orders.php
+++ views/admin/orders.php
@@ -13,60 +13,11 @@
   <!--  Body Wrapper -->
   <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
     data-sidebar-position="fixed" data-header-position="fixed">
-    <!-- Sidebar Start -->
-    <aside class="left-sidebar">
-        <!-- Sidebar scroll-->
-          <div class="brand-logo d-flex align-items-center justify-content-between">
-            <a href="./index.html" class="text-nowrap logo-img">
-              <img src="../../public/admin/assets/images/logos/dark-logo.svg" width="180" alt="" />
-            </a>
-            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-              <i class="ti ti-x fs-8"></i>
-            </div>
-          </div>
-           <!-- Sidebar navigation-->
-           <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-              <ul id="sidebarnav">
-                <li class="nav-small-cap">
-                  <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                  <span class="hide-menu">Home</span>
-                </li>
-                <li class="sidebar-item">
-                  <a class="sidebar-link" href="./index.php" aria-expanded="false">
-                    <span>
-                      <i class="ti ti-layout-dashboard"></i>
-                    </span>
-                    <span class="hide-menu">Dashboard</span>
-                  </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="./sales.php" aria-expanded="false">
-                      <span>
-                        <i class="ti ti-layout-dashboard"></i>
-                      </span>
-                      <span class="hide-menu">Sales</span>
-                    </a>
-                  </li>
-                  <li class="sidebar-item">
-                      <a class="sidebar-link" href="./products.php" aria-expanded="false">
-                        <span>
-                          <i class="ti ti-layout-dashboard"></i>
-                        </span>
-                        <span class="hide-menu">Products</span>
-                      </a>
-                    </li>
-                    <li class="sidebar-item">
-                      <a class="sidebar-link" href="./orders.php" aria-expanded="false">
-                        <span>
-                          <i class="ti ti-layout-dashboard"></i>
-                        </span>
-                        <span class="hide-menu">Orders</span>
-                      </a>
-                    </li>
-                  </ul>
-              </nav>
-        </aside>
-    <!--  Sidebar End -->
+
+    <?php
+            include('../partials/admin-sidenav.php')
+    ?>
+
     <!--  Main wrapper -->
     <div class="body-wrapper">
       <!--  Header Start -->
