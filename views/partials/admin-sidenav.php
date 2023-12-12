63cfc32a7b0dfddd2f274c938a8e17ebe0762e7f
DzeyEm <jmlaylo7@gmail.com>
2023-12-12 10:16:21 +0800

    Admin login done

--- views/partials/admin-sidenav.php
+++ views/partials/admin-sidenav.php
@@ -3,7 +3,7 @@
       <!-- Sidebar scroll-->
         <div class="brand-logo d-flex align-items-center justify-content-between">
           <a href="./index.html" class="text-nowrap logo-img">
-            <img src="../../public/admin/assets/images/logos/dark-logo.svg" width="180" alt="" />
+            <img src="../../public/assets/Untitled design1.png" width="180" alt="" />
           </a>
           <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
             <i class="ti ti-x fs-8"></i>
